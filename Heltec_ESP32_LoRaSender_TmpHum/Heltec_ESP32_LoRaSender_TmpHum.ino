15d1a4d642dfb38fc3d7f0c3727ce76943a23cc1
Paula Petcu <petcu.paula@gmail.com>
2017-10-15 16:08:39 +0200

    Disconnect from WiFi as we don't use it

--- Heltec_ESP32_LoRaSender_TmpHum/Heltec_ESP32_LoRaSender_TmpHum.ino
+++ Heltec_ESP32_LoRaSender_TmpHum/Heltec_ESP32_LoRaSender_TmpHum.ino
@@ -84,6 +84,7 @@ void setup(void)
   WiFi.macAddress(mac);
   sprintf(deviceId, "%2X%2X%2X%2X%2X%2X", mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
   Serial.println(deviceId);
+  WiFi.disconnect();
 }
 
 void loop(void)
